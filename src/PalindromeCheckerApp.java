3edc4bc20f6552047397055963332f4c6f8330e4
sanjiv-0103 <bp2411@srmist.edu.in>
2026-03-02 16:06:37 +0530

    UC11:Object Oriented

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,26 +4,39 @@ public class PalindromeCheckerApp {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a phrase to check if it is a palindrome: ");
+        System.out.print("Enter a word to check if it is a palindrome: ");
         String input = scanner.nextLine();
 
-        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        PalindromeService service = new PalindromeService();
 
-        boolean isPalindrome = true;
-
-        for (int i = 0; i < normalized.length() / 2; i++) {
-            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
+        boolean isPalindrome = service.checkPalindrome(input);
 
         if (isPalindrome) {
-            System.out.println("The phrase \"" + input + "\" is a palindrome (ignoring spaces and case).");
+            System.out.println("The string \"" + input + "\" is a palindrome.");
         } else {
-            System.out.println("The phrase \"" + input + "\" is NOT a palindrome.");
+            System.out.println("The string \"" + input + "\" is NOT a palindrome.");
         }
 
         scanner.close();
     }
+}
+
+class PalindromeService {
+
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
 }
