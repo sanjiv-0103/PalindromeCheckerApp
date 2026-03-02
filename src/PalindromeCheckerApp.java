3328aa79039012670401a58f558242bee5bec1f2
sanjiv-0103 <bp2411@srmist.edu.in>
2026-03-02 15:25:43 +0530

    UC4:Character Array Based

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,24 +1,35 @@
 import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a word to check if it is a palindrome: ");
-        String input = scanner.nextLine();
+            System.out.print("Enter a word to check if it is a palindrome: ");
+            String input = scanner.nextLine();
 
-        String reversed = "";
+            char[] chars = input.toCharArray();
 
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+            int start = 0;
+
+            int end = chars.length - 1;
+
+            boolean isPalindrome = true;
+
+            while (start < end) {
+
+                if (chars[start] != chars[end]) {
+                    isPalindrome = false;
+                    break;
+                }
+
+                start++;
+                end--;
+            }
+
+            if (isPalindrome) {
+                System.out.println("The string \"" + input + "\" is a palindrome.");
+            } else {
+                System.out.println("The string \"" + input + "\" is NOT a palindrome.");
+            }
+            scanner.close();
         }
-
-        if (input.equals(reversed)) {
-            System.out.println("The string \"" + input + "\" is a palindrome.");
-        } else {
-            System.out.println("The string \"" + input + "\" is NOT a palindrome.");
-        }
-
-        scanner.close();
-
-    }
 }
