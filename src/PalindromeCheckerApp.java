fe18c6d968fead2b0b66c3764833ab1d2745c2e5
sanjiv-0103 <bp2411@srmist.edu.in>
2026-03-02 14:31:44 +0530

    UC1: Added Welcome Message Module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,7 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initiated successfully.");
+    }
 }
